d4dd2969628a9811167d2a79df09c9a5f078d596
cristylk <cristy.koebler@gmail.com>
2015-06-28 15:08:18 -0400

    added edit and delete buttons

--- sinatra-ar/sinatra-mvc/app/controllers/people_controller.rb
+++ sinatra-ar/sinatra-mvc/app/controllers/people_controller.rb
@@ -3,6 +3,43 @@ get '/people' do
 	erb :"/people/index"
 end
 
+get '/people/new' do
+	@person = Person.new
+	erb :"/people/new"
+end
+
+post '/people' do
+	if params[:birthdate].include?("-")
+		birthdate = params[:birthdate]
+	else
+		birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+	end
+
+	person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: params[:birthdate])
+
+	redirect "/people/#{person.id}"
+end
+
+get '/people/:id/edit' do
+	@person = Person.find(params[:id])
+	erb :'/people/edit'
+end
+
+put '/people/:id' do
+	person = Person.find(params[:id])
+  	person.first_name = params[:first_name]
+  	person.last_name = params[:last_name]
+  	person.birthdate = params[:birthdate]
+  	person.save
+  	redirect "/people/#{person.id}"
+end
+
+delete '/people/:id' do
+	person = Person.find(params[:id])
+	person.delete
+	redirect "/people"
+end
+
 get '/people/:id' do
 	@person = Person.find(params[:id])
 	birthdate_string = @person.birthdate.strftime("%m%d%Y")
